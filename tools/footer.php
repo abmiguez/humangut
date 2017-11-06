7cf4c106a18314450129c460ad8784a22f3f29c2
mahmi <abmiguez@esei.uvigo.es>
2017-11-06 11:20:52 +0100

    Temporal fix

--- tools/footer.php
+++ tools/footer.php
@@ -3,10 +3,11 @@
 <br/></br>&copy; SING - Sistemas Inform&aacute;ticos de Nueva Generaci&oacute;n 2017</br></br></br>
 </div>
 
+
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-
+<script src="js/js.js"></script>
 <?php if(ereg("/listOmic.php$", $_SERVER['REQUEST_URI'])){ ?> 
 <script src="js/omic.js"></script>
 <script>
